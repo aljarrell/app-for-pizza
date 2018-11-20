a3d10f21ff5985094b2d4ba0c70df142bb43f475
aljarrell <alan.k.jarrell@gmail.com>
2018-11-20 15:28:34 -0500

    got the meat choices to pass through to confirm order page

--- app.rb
+++ app.rb
@@ -9,12 +9,14 @@ post '/home' do
   size = params[:size]
   crust = params[:crust]
   sauce = params[:sauce]
-  redirect 'confirm_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce
+  meats = params[:meats].join(", ")
+  redirect 'confirm_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats
 end
 
 get '/confirm_order' do
   size = params[:size]
   crust = params[:crust]
   sauce = params[:sauce]
-  erb :confirm_order, locals: {size: size, crust: crust, sauce: sauce}
+  meats = params[:meats]
+  erb :confirm_order, locals: {size: size, crust: crust, sauce: sauce, meats: meats}
 end
