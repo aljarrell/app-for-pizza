3ce8ef1db91d94d6e3a0c09cccdd491f687b4243
aljarrell <alan.k.jarrell@gmail.com>
2018-11-20 15:49:40 -0500

    added veggies and conditional for if no veggies was selected and made sure data passes to confirm page

--- app.rb
+++ app.rb
@@ -12,9 +12,15 @@ post '/home' do
   if params[:meats].include?("no meat")
     meats = "no meat"
   else
-  meats = params[:meats].join(", ")
+    meats = params[:meats].join(", ")
   end
-  redirect 'confirm_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats
+
+  if params[:veggies].include?("no veggies")
+    veggies = "no veggies"
+  else
+    veggies = params[:veggies].join(", ")
+  end
+  redirect 'confirm_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies
 end
 
 get '/confirm_order' do
@@ -22,5 +28,6 @@ get '/confirm_order' do
   crust = params[:crust]
   sauce = params[:sauce]
   meats = params[:meats]
-  erb :confirm_order, locals: {size: size, crust: crust, sauce: sauce, meats: meats}
+  veggies = params[:veggies]
+  erb :confirm_order, locals: {size: size, crust: crust, sauce: sauce, meats: meats, veggies: veggies}
 end
