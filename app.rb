26ed3001e524fb26be025ce6f0ecc6bb613e54ab
aljarrell <alan.k.jarrell@gmail.com>
2018-11-21 08:04:58 -0500

    refactored all variables into a single variable pizza_ingredients for readability

--- app.rb
+++ app.rb
@@ -14,7 +14,6 @@ post '/home' do
   else
     meats = "no meat"
   end
-
   if params[:veggies]
     veggies = params[:veggies].join(", ")
   else
@@ -27,71 +26,58 @@ get '/confirm_order' do
   size = params[:size]
   crust = params[:crust]
   sauce = params[:sauce]
-  meats = params[:meats]
-  veggies = params[:veggies]
-  erb :confirm_order, locals: {size: size, crust: crust, sauce: sauce, meats: meats, veggies: veggies}
+  if params[:meats]
+    meats = params[:meats]
+  else
+    meats = "no meat"
+  end
+  if params[:veggies]
+    veggies = params[:veggies]
+  else
+    veggies = "no veggies"
+  end
+  pizza_ingredients = Array.new
+  pizza_ingredients.push(size, crust, sauce, meats, veggies)
+  p "this is PIZZA ingredients #{pizza_ingredients}"
+  p "this is PARAMS #{params}"
+  erb :confirm_order, locals: {pizza_ingredients: pizza_ingredients}
 end
 
 post '/confirm_order' do
-  size = params[:size]
-  crust = params[:crust]
-  sauce = params[:sauce]
-  meats = params[:meats]
-  veggies = params[:veggies]
-  redirect 'delivery?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies
+  pizza_ingredients = params[:pizza_ingredients]
+  redirect 'delivery?pizza_ingredients=' + pizza_ingredients
 end
 
 get '/delivery' do
-  size = params[:size]
-  crust = params[:crust]
-  sauce = params[:sauce]
-  meats = params[:meats]
-  veggies = params[:veggies]
-  #p "this is GET params #{params}"
-  erb :delivery, locals: {size: size, crust: crust, sauce: sauce, meats: meats, veggies: veggies}
+  pizza_ingredients = params[:pizza_ingredients]
+  erb :delivery, locals: {pizza_ingredients: pizza_ingredients}
 end
 
 post '/delivery' do
-  size = params[:size]
-  crust = params[:crust]
-  sauce = params[:sauce]
-  meats = params[:meats]
-  veggies = params[:veggies]
+  pizza_ingredients = params[:pizza_ingredients]
   delivery_option = params[:delivery_option]
   #p "this is POST params #{params}"
   if params[:delivery_option] == "take out"
-    redirect '/final_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies + '&delivery_option=' + delivery_option
+    redirect '/final_order?pizza_ingredients=' + pizza_ingredients + '&delivery_option=' + delivery_option
   else
-    redirect '/address?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies + '&delivery_option=' + delivery_option
+    redirect '/address?pizza_ingredients=' + pizza_ingredients + '&delivery_option=' + delivery_option
   end
 end
 
 get '/address' do
-  size = params[:size]
-  crust = params[:crust]
-  sauce = params[:sauce]
-  meats = params[:meats]
-  veggies = params[:veggies]
+  pizza_ingredients = params[:pizza_ingredients]
   delivery_option = params[:delivery_option]
-  erb :address, locals: {size: size, crust: crust, sauce: sauce, meats: meats, veggies: veggies, delivery_option: delivery_option}
+  erb :address, locals: {pizza_ingredients: pizza_ingredients, delivery_option: delivery_option}
 end
 
 post '/address' do
-  size = params[:size]
-  crust = params[:crust]
-  sauce = params[:sauce]
-  meats = params[:meats]
-  veggies = params[:veggies]
+  pizza_ingredients = params[:pizza_ingredients]
   address = params[:address]
-  redirect '/final_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies + '&address=' + address
+  redirect '/final_order?pizza_ingredients=' + pizza_ingredients + '&address=' + address
 end
 
 get '/final_order' do
-  size = params[:size]
-  crust = params[:crust]
-  sauce = params[:sauce]
-  meats = params[:meats]
-  veggies = params[:veggies]
+  pizza_ingredients = params[:pizza_ingredients]
   address = params[:address]
-  erb :final_order, locals: {size: size, crust: crust, sauce: sauce, meats: meats, veggies: veggies, address: address}
+  erb :final_order, locals: {pizza_ingredients: pizza_ingredients, address: address}
 end
