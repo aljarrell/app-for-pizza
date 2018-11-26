658ff6a17b29dad3ec62bbeee8251b2bf0a8caf9
aljarrell <alan.k.jarrell@gmail.com>
2018-11-25 21:14:45 -0500

    got multiple pizzas to pass through

--- app.rb
+++ app.rb
@@ -6,23 +6,31 @@ get '/' do
 end
 
 post '/home' do
-  size = params[:size]
-  crust = params[:crust]
-  sauce = params[:sauce]
-  if params[:meats]
-    meats = params[:meats].join(", ")
-  else
-    meats = "no meat"
-  end
-  if params[:veggies]
-    veggies = params[:veggies].join(", ")
-  else
-    veggies = "no veggies"
-  end
-  redirect 'confirm_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies
+    number_of_pizzas = params[:number_of_pizzas]
+    size = params[:size]
+    crust = params[:crust]
+    sauce = params[:sauce]
+    if params[:meats]
+      meats = params[:meats].join(",")
+    else
+      meats = "no meat"
+    end
+    if params[:veggies]
+      veggies = params[:veggies].join(",")
+    else
+      veggies = "no veggies"
+    end
+    order_choice = params[:order_choice]
+    if order_choice == "Continue to Confirm Order!"
+      redirect 'confirm_order?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies + '&number_of_pizzas=' + number_of_pizzas
+    else
+       redirect '/?size=' + size + '&crust=' + crust + '&sauce=' + sauce + '&meats=' + meats + '&veggies=' + veggies + '&number_of_pizzas=' + number_of_pizzas
+    end
 end
 
 get '/confirm_order' do
+  number_of_pizzas = params[:number_of_pizzas].to_i
+  #p "this is NUMBER OF PIZZAS #{number_of_pizzas}"
   size = params[:size]
   crust = params[:crust]
   sauce = params[:sauce]
@@ -36,20 +44,30 @@ get '/confirm_order' do
   else
     veggies = "no veggies"
   end
+  temp_arr = Array.new
   pizza_ingredients = Array.new
-  pizza_ingredients.push(size, crust, sauce, meats, veggies)
-  p "this is PIZZA ingredients #{pizza_ingredients}"
-  p "this is PARAMS #{params}"
-  erb :confirm_order, locals: {pizza_ingredients: pizza_ingredients}
+  temp_arr.push(size, crust, sauce, meats, veggies)
+  number_of_pizzas.times do
+    #p "BEFORE PUSH #{pizza_ingredients}"
+    pizza_ingredients.push(temp_arr).join(",")
+    #p "AFTER PUSH #{pizza_ingredients}"
+  end
+  #p "this is PIZZA ingredients #{pizza_ingredients}"
+  #p "this is PARAMS #{params}"
+  erb :confirm_order, locals: {pizza_ingredients: pizza_ingredients, number_of_pizzas: number_of_pizzas}
 end
 
 post '/confirm_order' do
+  number_of_pizzas = params[:number_of_pizzas]
   pizza_ingredients = params[:pizza_ingredients]
-  redirect '/delivery?pizza_ingredients=' + pizza_ingredients
+    p "this is PIZZA ingredients #{pizza_ingredients}"
+    p "this is NUMBER #{number_of_pizzas}"
+  redirect '/delivery?pizza_ingredients=' + pizza_ingredients + '&number_of_pizzas=' + number_of_pizzas
 end
 
 get '/delivery' do
   pizza_ingredients = params[:pizza_ingredients]
+  p "this is PIZZA ingredients #{pizza_ingredients}"
   erb :delivery, locals: {pizza_ingredients: pizza_ingredients}
 end
 
